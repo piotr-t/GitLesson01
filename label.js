c2f6f698c41c1dae14d7999fc08d125e511fefe0
Adam Zaręba <adam.d.zareba@gmail.com>
2019-03-02 19:50:30 +0100

    Updated label script. Green color support only

--- label.js
+++ label.js
@@ -1,5 +1,5 @@
 console.log("This is the best label");
 
-console.log("Label displays random text color");
+console.log("Label displays green text color");
 console.log("Label converts string to uppercase");
 
